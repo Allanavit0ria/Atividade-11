b2758eb7b91de7bef4d62c0d7c07456e3029d882
Allanavit0ria <132669005+Allanavit0ria@users.noreply.github.com>
2023-08-11 11:47:50 +0000

    a

--- bd.js
+++ bd.js
@@ -24,5 +24,13 @@ async function selectUsuario(id) {
   return res.rows;
 }
 //bd.js
-export { selectUsuarios, selectUsuario }; 
+export { selectUsuarios, selectUsuario, insertUsuario };  
+
+//bd.js
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+}
 
